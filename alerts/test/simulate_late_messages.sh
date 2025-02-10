2ae6308e9b37f803d5df41fcfa2ede3ea1ab56ad
Enkhbold Ochirsuren <E.Ochirsuren@gsi.de>
2025-02-10 15:28:47 +0100

    alerts: alert rules for the test and production (TOS) dashboards

---
+++ alerts/test/simulate_late_messages.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# Simulate the late messages (up to maxlate) and
+# send it to a Grafana server (using host and port) in given interval (interval).
+
+# simulate late messages
+rmin=1
+rmax=20
+maxlate=5
+nlate=0
+
+# temperature metric name
+metric="test.nlate"
+
+# Graphite server: host=localhost, port=2003
+host="localhost"
+port=2003
+
+# interval (seconds)
+interval=60
+
+while :
+do
+    data=$nlate
+    timestamp=$(date +%s)
+    echo "$(date -d @$timestamp +"%F %T") $data"
+    echo "$metric $data $timestamp" | nc -u $host $port
+
+    sleep $interval
+    random=$(( $RANDOM%($rmax-$rmin+1)+$rmin ))
+    echo "random: $random"
+    if [ $random -le $maxlate ]; then
+        nlate=$(( nlate + random ))
+    fi
+done
+
+# Setting for testing the Grafana alert
+
+# A. Query
+# range = 5m, interval = 1m
+# functions = derivative() + keepLastValue()
+
+# B. Classic condition
+# conditions: when = last() of = A, is above = 0
+
